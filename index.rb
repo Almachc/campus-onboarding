93cf4232ad95ad773d8bb72a971c50a41eebd1be
Alexandre Machado <alexandre.machcruz@gmail.com>
2020-05-10 21:40:51 -0300

    WIP Desafio 3

--- index.rb
+++ index.rb
@@ -1,6 +1,7 @@
 require_relative 'ibgeApi'
 require_relative 'ibgeDB'
 require 'terminal-table'
+require 'csv'
 
 def choose_names
   print 'Digite um ou mais nomes, separados por vírgula, a serem pesquisados: '
@@ -43,27 +44,26 @@ def generate_name_frequency_table(names)
   puts Terminal::Table.new :headings => heading, :rows => rows
 end
 
-def generate_rankings(location_id:)
-  ranking_geral = IbgeApi.get_ranking_of_names_by(location_id: location_id)
-  ranking_masc = IbgeApi.get_ranking_of_names_by(location_id: location_id, gender: 'M')
-  ranking_fem = IbgeApi.get_ranking_of_names_by(location_id: location_id, gender: 'F')
+def generate_ranking(location_id: '', gender: '')
+  title = 'Ranking Geral'
+  title = gender == 'M' ? 'Ranking Masculino' : 'Ranking Feminino' if !gender.empty?
+  
+  ranking = IbgeApi.get_ranking_of_names_by(location_id: location_id, gender: gender)
 
-  heading = ['Ranking Geral', 'Ranking Masculino', 'Ranking Feminino']
-  rows = Array.new(ranking_geral[:res].length) {[]}
+  heading = ['Pos', 'Nome', 'Frequência', 'Porcentagem']
+  rows = Array.new(ranking[:res].length) {[]}
 
-  ranking_geral[:res].each_with_index {|item, index| 
-    rows[index] << "#{item[:ranking]} - #{item[:nome]}"
+  populacao = teste(location_id: location_id)
+
+  ranking[:res].each_with_index {|item, index| 
+    porcentagem = (item[:frequencia] * 100) / populacao.to_f
+    rows[index] << item[:ranking]
+    rows[index] << item[:nome]
+    rows[index] << item[:frequencia]
+    rows[index] << "#{porcentagem.round(2)}%"
   }
  
-  ranking_masc[:res].each_with_index {|item, index| 
-    rows[index] << "#{item[:ranking]} - #{item[:nome]}"
-  }
-  
-  ranking_fem[:res].each_with_index {|item, index| 
-    rows[index] << "#{item[:ranking]} - #{item[:nome]}"
-  }
-  
-  puts Terminal::Table.new :headings => heading, :rows => rows
+  Terminal::Table.new :title => title, :headings => heading, :rows => rows
 end
 
 def menu
@@ -82,6 +82,17 @@ def welcome
   puts 'BEM VINDO AO SISTEMA DE NOMES DO IBGE!!!'
 end
 
+def teste(location_id:)
+  populacao = nil
+  CSV.foreach("assets/populacao_2019.csv", headers: true) do |row|
+    if row["Cód."].to_i == location_id
+      populacao = row["População Residente - 2019"] 
+      break
+    end
+  end
+  populacao.to_i
+end
+
 def startApplication
   welcome
   option = menu
@@ -90,7 +101,10 @@ def startApplication
     if option == 1
       state_initials = choose_a_state
       result = IbgeDB::Estado.find_by_sql("SELECT id FROM estados WHERE sigla = \"#{state_initials.upcase}\"")
-      generate_rankings(location_id: result[0][:id])
+      geral = generate_ranking(location_id: result[0][:id])
+      masc = generate_ranking(location_id: result[0][:id], gender: "M")
+      fem = generate_ranking(location_id: result[0][:id], gender: "F")
+      puts Terminal::Table.new :rows => [[geral, masc, fem]]
     elsif option == 2
       city_name = choose_a_city
       result = IbgeDB::Cidade.find_by_sql("SELECT id FROM cidades WHERE nome = \"#{city_name}\"")
